65ecf40e8d19ee399ead39a0ed6882de8fe1a032
Lucas Couto <loc.unb@gmail.com>
2023-06-04 00:07:06 -0300

    WelcomeComponent as standalone component

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,7 +1,10 @@
+import { DetailsComponent } from './details/details.component';
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
+  imports: [DetailsComponent],
   selector: 'app-welcome',
-  templateUrl: './welcome.component.html'
+  templateUrl: './welcome.component.html',
 })
 export class WelcomeComponent {}
